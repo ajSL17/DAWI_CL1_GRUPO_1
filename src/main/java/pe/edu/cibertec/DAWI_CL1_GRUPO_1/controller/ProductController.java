89ff3f47abed0f93c0cf81c3010d0e418822c653
enzolg03 <enzojoaquinlimaygallo@gmail.com>
2023-09-30 18:38:30 -0500

    segundo comit de enzolimay

--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/controller/ProductController.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/controller/ProductController.java
@@ -1,4 +1,28 @@
 package pe.edu.cibertec.DAWI_CL1_GRUPO_1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.DAWI_CL1_GRUPO_1.model.ProductModel;
+
+@Controller
 public class ProductController {
+    @GetMapping("/calcularTotal")
+    public String index(Model model){
+        model.addAttribute("productmodel", new ProductModel());
+        model.addAttribute("verresultado", false);
+        return "formProduct";
+    }
+    @PostMapping("/calcularTotal")
+    public String calcularTotal(@ModelAttribute("productmodel") ProductModel productmodel, Model model){
+        Double total = productmodel.getCantidad() * productmodel.getPrecio();
+        if (total > 200) {
+            total = total * 0.8;  // Aplicar descuento del 20%
+        }
+        model.addAttribute("verresultado", true);
+        model.addAttribute("resultado", "El total a pagar es: $" + String.format("%.2f", total));
+        return "formProduct";
+    }
 }
