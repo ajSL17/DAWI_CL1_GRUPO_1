472d0172c5a0b9c0ad18184bd4e9bb1ef770d52a
AlexHc26 <mahc2601@gmail.com>
2023-09-30 14:54:30 -0500

    segundo commit de manuel

--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/controller/GradesController.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/controller/GradesController.java
@@ -1,4 +1,28 @@
 package pe.edu.cibertec.DAWI_CL1_GRUPO_1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.DAWI_CL1_GRUPO_1.model.GradesModel;
+
+import java.util.Arrays;
+@Controller
 public class GradesController {
+    @GetMapping("/calcularPromedio")
+    public String index(Model model){
+        model.addAttribute("gradesmodel", new GradesModel());
+        model.addAttribute("verresultado", false);
+        return "formGrades";
+    }
+    @PostMapping("/calcularPromedio")
+    public String calcularPromedio(@ModelAttribute("gradesmodel") GradesModel gradesmodel, Model model){
+        Double[] notas = {gradesmodel.getNota1(), gradesmodel.getNota2(), gradesmodel.getNota3(), gradesmodel.getNota4()};
+        Arrays.sort(notas);
+        Double promedio = notas[1]*0.2 + notas[2]*0.3 + notas[3]*0.5;
+        model.addAttribute("verresultado", true);
+        model.addAttribute("resultado", "El promedio es: " + String.format("%.2f", promedio));
+        return "formGrades";
+    }
 }
--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/model/GradesModel.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/model/GradesModel.java
@@ -1,4 +1,17 @@
 package pe.edu.cibertec.DAWI_CL1_GRUPO_1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GradesModel {
+    private Double nota1;
+    private Double nota2;
+    private Double nota3;
+    private Double nota4;
 }
