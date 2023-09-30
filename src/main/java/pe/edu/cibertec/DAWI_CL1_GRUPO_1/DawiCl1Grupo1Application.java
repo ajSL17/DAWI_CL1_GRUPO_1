f5e9e402758919cdb73eeefd4aa324b02a6e725c
Enzo008 <enzoaguirre629@gmail.com>
2023-09-30 10:57:59 -0500

    Iniciando proyecto

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/DawiCl1Grupo1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_GRUPO_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1Grupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL1_GRUPO_1/DawiCl1Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_GRUPO_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
